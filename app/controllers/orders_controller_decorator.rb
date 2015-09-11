cb83928d7bf0e63495822d3045b626c44b8e01b6
Don Sullivan <apologeticdon@gmail.com>
2015-09-10 22:08:00 -0400

    first commit.

---
+++ app/controllers/orders_controller_decorator.rb
@@ -0,0 +1,32 @@
+Spree::OrdersController.class_eval do
+
+  def populate
+    order    = current_order(create_order_if_necessary: true)
+    
+    @product = Spree::Product.find(params[:products].keys[0])
+    variant  = @product.identified_variant(params[:product].values.map{ |v| v.to_i })
+    quantity = params[:quantity].to_i
+    options  = params[:options] || {}
+
+    # 2,147,483,647 is crazy. See issue #2695.
+    if quantity.between?(1, 2_147_483_647)
+      begin
+        order.contents.add(variant, quantity, options)
+      rescue ActiveRecord::RecordInvalid => e
+        error = e.record.errors.full_messages.join(", ")
+      end
+    else
+      error = Spree.t(:please_enter_reasonable_quantity)
+    end
+
+    if error
+      flash[:error] = error
+      redirect_back_or_default(spree.root_path)
+    else
+      respond_with(order) do |format|
+        format.html { redirect_to cart_path }
+      end
+    end
+  end  
+
+end
---
+++ app/models/spree/variant_decorator.rb
@@ -0,0 +1,15 @@
+Spree::Variant.class_eval do
+  
+  include ActionView::Helpers::NumberHelper
+  
+  def to_hash
+    actual_price  = self.price
+    #actual_price += Calculator::Vat.calculate_tax_on(self) if Spree::Config[:show_price_inc_vat]
+    { 
+      :id    => self.id, 
+      :count => self.stock_items_count, 
+      :price => number_to_currency(actual_price)
+    }
+  end
+    
+end
